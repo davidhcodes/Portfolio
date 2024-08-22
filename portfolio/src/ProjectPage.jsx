60d7120f8f8807fdab43d80a306e86b8427d503e
davidhcodes <dhanlyg1@gmail.com>
2024-08-22 11:01:09 +0100

    Updates the project page for mobile first design

--- portfolio/src/ProjectPage.jsx
+++ portfolio/src/ProjectPage.jsx
@@ -14,66 +14,76 @@ import QuizGame from "./assets/quizgame.png";
 
 export default function ProjectPage() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-l from-indigo-950 to-blue-950">
-      <div className="col-span-5 row-start-1 row-end-5 w-full bg-blue-950 flex justify-center space-x-20 py-5">
+    <div className=" m-auto grid grid-rows-20 grid-cols-2  mb-24  bg-gradient-to-l from-indigo-950 to-blue-950  md:grid-rows-20 md:grid-cols-5 md:w-full ">
+      <div className="bg-blue-950 row-start-1 row-end-8 col-span-2 space-x-10 items-center justify-center flex   py-4  md:row-start-1 md:row-end-5 md:col-span-5 md:bg-blue-950 md:flex md:justify-center md:space-x-20 md:py-4">
         <Banner />
       </div>
-      <div className="w-full max-w-screen-lg m-auto grid grid-rows-20 grid-cols-2 gap-2 mb-24 md:grid-rows-20 md:grid-cols-5">
-        <div className="col-span-5 row-start-5 row-end-12 w-full min-h-min flex flex-col justify-between items-center">
-          <h1 className="text-3xl text-white font-bold flex font-sans shadow-md px-8 py-1">
+      <div className="w-full max-w-screen-lg col-span-2 row-start-8 row-end-20  md:grid-rows-20 md:w-full md:max-w-screen-lg md:m-auto md:grid md:grid-rows-20 md:grid-cols-5 md:gap-2 md:mb-24">
+        <div className="col-span-5 row-start-5 row-end-12 w-full min-h-min flex flex-col justify-between items-center md:col-span-5 md:row-start-5 md:row-end-12 md:w-full md:min-h-min md:flex md:flex-col md:justify-between md:items-center">
+          <h1 className="text-3xl text-white font-bold flex font-sans shadow-md px-8 py-1 md:text-3xl md:text-white md:font-bold md:flex md:font-sans md:shadow-md md:px-8 md:py-1">
             Projects
           </h1>
-          <h2 className="text-xl text-white font-bold flex font-sans shadow-md px-8 py-1">
+          <h2 className="text-xl text-center text-white font-bold flex font-sans shadow-md px-8 py-1 md:text-xl md:text-center md:text-white md:font-bold md:flex md:font-sans md:shadow-md md:px-8 md:py-1">
             (Any websites will take a few minutes to load)
           </h2>
-          <div className="grid grid-cols-3 grid-rows-5 gap-8 mt-5">
-            <div className="pt-2">
-              <div className="flex items-center justify-center">
-                <h2 className="pb-2 ml-8  text-xl font-semibold text-white text-center">
+          <div className="grid grid-cols-1 grid-rows-5 gap-8 p-4 mt-5 md:grid md:grid-cols-1 md:grid-rows-5 md:gap-8 md:p-4 md:mt-5">
+            <div className="pt-2 md:pt-2">
+              <div className="flex items-center justify-center md:flex md:items-center md:justify-center">
+                <h2 className="pb-2 ml-8  text-xl font-semibold text-white text-center md:pb-2 md:ml-8  md:text-xl md:font-semibold md:text-white md:text-center">
                   HG News App
                 </h2>
-                <div className="flex">
-                  <img src={ReactLogo} className="w-24 pb-1" alt="React logo" />
+                <div className="flex md:flex">
+                  <img
+                    src={ReactLogo}
+                    className="w-24 pb-1 md:w-24 md:pb-1"
+                    alt="React logo"
+                  />
                   <img
                     src={JSLogo}
-                    className="w-14 h-14 mt-2"
+                    className="w-14 h-14 mt-2 md:w-14 md:h-14 md:mt-2"
                     alt="JavaScript logo"
                   />
                 </div>
               </div>
-              <img className="h-48  " src={HGNews} />
-              <div className="flex space-x-4 mt-2 items-center justify-center">
+              <img className="h-48 md:h-48  " src={HGNews} />
+              <div className="flex space-x-4 mt-2 items-center justify-center md:flex md:space-x-4 md:mt-2 md:items-center md:justify-center">
                 <a href="https://github.com/davidhcodes/nc-news-app">
-                  <img className="w-36" src={Github} />
+                  <img className="w-36 md:w-36" src={Github} />
                 </a>
                 <a href="https://hg-news.netlify.app/">
-                  <button className="bg-gradient-to-l from-blue-800 to-blue-700 rounded-xl shadow-lg w-36 h-16 ">
-                    <h2 className="text-white text-2xl"> Website </h2>
+                  <button className="bg-gradient-to-l from-blue-800 to-blue-700 rounded-xl shadow-lg w-36 h-16 md:bg-gradient-to-l md:from-blue-800 md:to-blue-700 md:rounded-xl md:shadow-lg md:w-36 md:h-16 ">
+                    <h2 className="text-white text-2xl md:text-white md:text-2xl">
+                      Website
+                    </h2>
                   </button>
                 </a>
               </div>
-              <p className="text-white p-2">
+              <p className="text-white p-2 md:text-white md:p-2">
                 An interactive news website, allowing users to read, comment and
                 vote on news articles.
               </p>
             </div>
 
-            <div className="items-center justify-center">
-              <div className="flex items-center justify-center">
-                <h2 className="pb-2 ml-8 text-xl font-semibold text-white text-center">
+            <div className="items-center justify-center md:items-center md:justify-center">
+              <div className="flex items-center justify-center md:flex md:items-center md:justify-center">
+                <h2 className="pb-2 ml-8 text-xl font-semibold text-white text-center md:pb-2 md:ml-8 md:text-xl md:font-semibold md:text-white md:text-center">
                   Popcorner App
                 </h2>
-                <div className="flex">
-                  <div className="flex-col">
-                    <img src={ReactLogo} className="w-24" alt="React logo" />
-                    <h3 className="text-react-native-blue text-xxs text-center justify-center items-center">
+                <div className="flex md:flex">
+                  <div className="flex-col md:flex-col">
+                    <img
+                      src={ReactLogo}
+                      className="w-24 md:w-24"
+                      alt="React logo"
+                    />
+                    <h3 className="text-react-native-blue text-xxs text-center justify-center items-center md:text-react-native-blue  md:text-xxs  md:text-center  md:ustify-center  md:items-center">
                       React Native
                     </h3>
                   </div>
 
                   <img
                     src={JSLogo}
-                    className="w-14 h-14 mt-2"
+                    className="w-14 h-14 mt-2 md:w-14 md:h-14 md:mt-2"
                     alt="JavaScript logo"
                   />
                 </div>
@@ -88,7 +98,7 @@ export default function ProjectPage() {
                   controls={true}
                 />
               </div>
-              <div className="flex space-x-4 mt-2 items-center justify-center">
+              <div className="flex space-x-4 mt-2 items-center justify-center md:flex md:space-x-4 md:mt-2 md:items-center md:justify-center">
                 <a href="https://github.com/davidhcodes/front-end-popcorner/">
                   <img className="w-36" src={GithubF} />
                 </a>
@@ -97,7 +107,7 @@ export default function ProjectPage() {
                 </a>
               </div>
               <div className="">
-                <p className="text-white p-2 -center">
+                <p className="text-white p-2 -center md:text-white  md:p-2 md:-center">
                   PopCorner is an app created to connect movie enthusiasts in a
                   community-based app, users can securely create a profile or
                   log-in, register interests, create and join communities and
@@ -106,57 +116,67 @@ export default function ProjectPage() {
                 </p>
               </div>
             </div>
-            <div className="pt-2">
-              <div className="flex items-center justify-center">
-                <h2 className="pb-2 ml-8  text-xl font-semibold text-white text-center">
+            <div className="pt-2 md:pt-2">
+              <div className="flex items-center justify-center md:flex md:items-center md:justify-center">
+                <h2 className="pb-2 ml-8  text-xl font-semibold text-white text-center md:pb-2 md:ml-8  md:text-xl md:font-semibold md:text-white md:text-center">
                   Weather App
                 </h2>
-                <div className="flex">
-                  <img src={ReactLogo} className="w-24 pb-1" alt="React logo" />
+                <div className="flex md:flex">
+                  <img
+                    src={ReactLogo}
+                    className="w-24 pb-1 md:w-24 md:pb-1"
+                    alt="React logo"
+                  />
                   <img
                     src={JSLogo}
-                    className="w-14 h-14 mt-2"
+                    className="w-14 h-14 mt-2 md:w-14 md:h-14 md:mt-2"
                     alt="JavaScript logo"
                   />
                 </div>
               </div>
-              <img className="h-48  " src={WeatherApp} />
-              <div className="flex space-x-4 mt-2 items-center justify-center">
+              <img className="h-48 md:h-48 " src={WeatherApp} />
+              <div className="flex space-x-4 mt-2 items-center justify-center md:flex md:space-x-4 md:mt-2 md:items-center md:justify-center">
                 <a href="https://github.com/davidhcodes/WeatherApp">
-                  <img className="w-36" src={Github} />
+                  <img className="w-36 md:w-36" src={Github} />
                 </a>
                 <a href="https://davidhcodesweather.netlify.app/">
-                  <button className="bg-gradient-to-l from-blue-800 to-blue-700 rounded-xl shadow-lg w-36 h-16 ">
-                    <h2 className="text-white text-2xl"> Website </h2>
+                  <button className="bg-gradient-to-l from-blue-800 to-blue-700 rounded-xl shadow-lg w-36 h-16  md:bg-gradient-to-l  md:from-blue-800  md:to-blue-700  md:rounded-xl  md:shadow-lg  md:w-36  md:h-16 ">
+                    <h2 className="text-white text-2xl md:text-white md:text-2xl">
+                      Website
+                    </h2>
                   </button>
                 </a>
               </div>
-              <p className="text-white p-2">
+              <p className="text-white p-2 md:text-white md:p-2">
                 An interactive weather app that displays the current weather and
                 5 day weather forecast for any city the user enters.
               </p>
             </div>
-            <div className="pt-2">
-              <div className="flex items-center justify-center">
-                <h2 className="pb-2   text-xl font-semibold text-white text-center">
+            <div className="pt-2 md:pt-2">
+              <div className="flex items-center justify-center md:flex md:items-center md:justify-center">
+                <h2 className="pb-2   text-xl font-semibold text-white text-center md:pb-2   md:text-xl md:font-semibold md:text-white md:text-center">
                   Interactive Quiz
                 </h2>
-                <div className="flex">
-                  <img src={ReactLogo} className="w-24 pb-1" alt="React logo" />
+                <div className="flex md:flex">
+                  <img
+                    src={ReactLogo}
+                    className="w-24 pb-1 md:w-24 md:pb-1"
+                    alt="React logo"
+                  />
                   <img
                     src={TSLogo}
-                    className="w-14 h-14 mt-2"
+                    className="w-14 h-14 mt-2 md:w-14 md:h-14 md:mt-2"
                     alt="TypeScript logo"
                   />
                 </div>
               </div>
-              <h2 className="pb-2  bg-black my-2 text-sm font-semibold text-white text-center">
+              <h2 className="pb-2  bg-black my-2 text-sm font-semibold text-white text-center md:pb-2  md:bg-black md:my-2 md:text-sm md:font-semibold md:text-white md:text-center">
                 Still in progress
               </h2>
-              <img className="h-48  " src={QuizGame} />
-              <div className="flex space-x-4 mt-2 items-center justify-center">
+              <img className="h-48  md:h-48" src={QuizGame} />
+              <div className="flex space-x-4 mt-2 items-center justify-center md:flex md:space-x-4 md:mt-2 md:items-center md:justify-center">
                 <a href="https://github.com/davidhcodes/QuizzApp">
-                  <img className="w-36" src={Github} />
+                  <img className="w-36 md:w-36" src={Github} />
                 </a>
                 {/* <a href="https://davidhcodesweather.netlify.app/">
                   <button className="bg-gradient-to-l from-blue-800 to-blue-700 rounded-xl shadow-lg w-36 h-16 ">
@@ -164,7 +184,7 @@ export default function ProjectPage() {
                   </button>
                 </a> */}
               </div>
-              <p className="text-white p-2">
+              <p className="text-white p-2 md:text-white md:p-2">
                 An interactive quiz with multiple choice answers, it displays
                 the user's results at the end.
               </p>
